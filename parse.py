de6c24fc6ba8c69d269564544a7aac622d5aa796
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-08-10 14:16:40 -0400

    commit on master

---
+++ parse.py
@@ -0,0 +1,5 @@
+# Creator: Christian Nell
+# Date: 8/10/20
+# Purpose:  A program that determines if a message (a text string) is a valid 
+#   SMTP “MAIL FROM” message. This is a message that tells a mail server who 
+#   (which user) is trying to send an email message. 
