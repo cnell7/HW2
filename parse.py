12925ed4512dbcc1e93a95df5296584941727700
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-08-17 13:52:11 -0400

    Added new test cases and fixed whitespace issue.

--- parse.py
+++ parse.py
@@ -13,52 +13,59 @@
 #   Output: None, will close program if the string is over
 #               and therefore incomplete before end of the
 #               SSMTP check
+
+
 def length_check(i, string):
     if i > (len(string) - 1):
         print("ERROR -- incomplete input")
-        exit()
+        return exit()
 
-    #   Purpose:
-    #   Input: Counter position code is at, and the string it
-    #               is checking
-    #   Output: Will return the string position, i, that
+#   Purpose:
+#   Input: Counter position code is at, and the string it
+#               is checking
+#   Output: Will return the string position, i, that
+
+
+def exit():
+    return False
 
 
 def mail_from_cmd(string):
     mailString = "MAIL"
     fromString = "FROM:"
-
     #   "MAIL"
     if(not(mailString == string[0:4])):
         print("ERROR -- mail-from-cmd")
-        exit()
-
-    i = 5
+        return exit()
+    i = 4
     #    <whitespace>
+    if(not(whitespace(i, string))):
+        return exit()
     i = whitespace(i, string)
-
     #   "FROM:"
     if(not(fromString == string[i:i+5])):
         print("ERROR -- mail-from-cmd")
-        exit()
-
+        return exit()
     i += 5
     #    <nullspace>
     i = nullspace(i, string)
-
+    if (nullspace(i, string) == False):
+        return exit()
     #   <reverse-path>
-    i = reverse_path(i, string)
-
+    if (reverse_path(i, string) == False):
+        return exit()
     #    <nullspace>
     i = nullspace(i, string)
-
     #   <CLRF>
     CRLF(i, string)
-
     print("Sender ok")
+    return True
 
 
 def whitespace(i, string):
+    if(SP(i, string) == False):
+        print("ERROR -- whitespace")
+        return False
     while SP(i, string):
         i += 1
         length_check(i, string)
@@ -73,6 +80,10 @@ def SP(i, string):
 
 
 def nullspace(i, string):
+    if(null(i, string) == True):
+        return False
+    if(not(SP(i, string))):
+        return i
     i = whitespace(i, string)
     return i
 
@@ -80,7 +91,9 @@ def nullspace(i, string):
 
 
 def null(i, string):
-    return null
+    if (length_check(i, string) == False):
+        return True
+    return False
 
 
 def reverse_path(i, string):
@@ -91,7 +104,7 @@ def path(i, string):
     #   "<"
     if string[i] != "<":
         print("ERROR -- path")
-        exit()
+        return exit()
 
     i += 1
 
@@ -99,17 +112,18 @@ def path(i, string):
     length_check(i, string)
 
     #    <mailbox>
-    mailbox(i, string)
+    if (mailbox(i, string) == False):
+        return False
     return i
 
 
 def mailbox(i, string):
-    local_part(i, string)
+    if (local_part(i, string) == False):
+        return False
+    i = 1 + local_part(i, string)
 
-    i += 3
-
-    domain(i, string)
-
+    if (domain(i, string) == False):
+        return False
     return i
 
 
@@ -119,15 +133,15 @@ def local_part(i, string):
     while string[i] != "@":
         if (string_(i, string)):
             print("ERROR -- local-part")
-            exit()
+            return exit()
         if i == (len(string) - 1):
             print("ERROR -- no @")
-            exit()
+            return exit()
         i += 1
 
     if local_part_start == i:
         print("ERROR -- local-part")
-        exit()
+        return exit()
 
     local_part = string[local_part_start:i]
     return i
@@ -150,7 +164,7 @@ def domain(i, string):
     while i != (len(string)):
         if special(i, string):
             print("ERROR -- domain")
-            exit()
+            return exit()
         i += 1
 
     domain = string[domain_start:i]
@@ -196,21 +210,49 @@ def special(i, string):
 
 def main():
     # Get user input from keyboard
-    #mail_from = raw_input()
+    # mail_from = raw_input()
 
     pass1 = "MAIL FROM:<he@h"
     pass2 = "MAIL  FROM:<eh@h"
     pass3 = "MAIL  FROM: <he@h"
+    pass4 = "MAIL        FROM:       <heh@h"
+
     fail1 = "mAIL FROM:<he@h"
     fail2 = "MAIL fROM:<he@h"
     fail3 = "MAIL FROM:< he@h"
+    fail4 = " MAIL FROM:<heh@h"
+    fail5 = "MAIL    FROM:"
+    fail6 = "MAILFROM:<"
+    fail7 = "MAIL ! FROM:<hi@hi"
+    fail8 = "MAIL! FROM:<hi@hi"
 
     print("pass")
+    print("1")
     mail_from_cmd(pass1)
+    print("2")
     mail_from_cmd(pass2)
+    print("3")
     mail_from_cmd(pass3)
+    print("4")
+    mail_from_cmd(pass4)
 
-    print("fail")
+    print("\nfail")
+    print("1 = mail from")
+    mail_from_cmd(fail1)
+    print("2 = mail from")
+    mail_from_cmd(fail2)
+    print("3 = local part")
+    mail_from_cmd(fail3)
+    print("4 = mail from")
+    mail_from_cmd(fail4)
+    print("5 = incomplete")
+    mail_from_cmd(fail5)
+    print("6 = whitespace")
+    mail_from_cmd(fail6)
+    print("7 = mail from")
+    mail_from_cmd(fail7)
+    print("8 = whitespace")
+    mail_from_cmd(fail8)
 
 
 main()
