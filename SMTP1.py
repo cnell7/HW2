fee8ee74aebd731db9bde30eca040be48dd7fe36
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-09-03 14:30:32 -0400

    Updated to server current edition

--- SMTP1.py
+++ SMTP1.py
@@ -388,7 +388,7 @@ def call_command(string, count):
     elif(check_data(string) != False):
         passCommand = check_data(string)
         if(count < 2):
-            return error503(string)
+             return error503(string)
         echo(string)
         print("354 Start mail input; end with <CRLF>.<CRLF>")
         count = -1
@@ -413,6 +413,20 @@ def writeData():
         f.close()
     ok250(0)
     return 0
+
+#   Skips incorrect command so rest of line can be tested
+'''
+def skipIncorrect(string):
+    i = 0
+    while (string != False):
+         if(string[i] == '\n'):
+              break
+         if(string[i] == '\\'):
+              if(string[i+1] == 'n'):
+              break
+         i += 1
+    return string[i:]
+''' 
 
 #   500 Syntax error
 
@@ -452,9 +466,9 @@ def main():
     for line in sys.stdin:
         count = call_command(line, count)
         if(not(count)):  # False = start over from MAIL FROM command
-            datas = []
-            mailboxs = []
-            rcpts = []
+            datas.clear()
+            mailboxs.clear()
+            rcpts.clear()
             count = 0
     if(count != 0):
         error501("Incomplete data input")
